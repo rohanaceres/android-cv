46c5c2822b961aac1bd54bde6da71f0fb6149d1d
Ceres Rohana <ceres.rohana@gmail.com>
2017-12-10 12:18:45 -0200

    First 2 activities for professiona info input.

--- app/src/main/java/rohanaceres/github/io/mycv/MainActivity.kt
+++ app/src/main/java/rohanaceres/github/io/mycv/MainActivity.kt
@@ -2,11 +2,34 @@ package rohanaceres.github.io.mycv
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun nextForm(view: View) {
+        val intent = Intent(this, Main2Activity::class.java)
+        val nome = findViewById<EditText>(R.id.editTextNome)
+        val email = findViewById<EditText>(R.id.editTextEmail)
+        val estado = findViewById<EditText>(R.id.editTextEstado)
+        val cidade = findViewById<EditText>(R.id.editTextCidade)
+        val telefone = findViewById<EditText>(R.id.editTextTelefone)
+
+        if (!(nome.text.toString() == "" || email.text.toString() == "" || estado.text.toString() == "" || cidade.text.toString() == "" || telefone.text.toString() == "")) {
+            val data = PersonalData.getInstance()
+            data.name = nome.getText().toString()
+            data.email = email.getText().toString()
+            data.district  = estado.getText().toString()
+            data.city  = cidade.getText().toString()
+            data.phoneNumber = telefone.getText().toString()
+
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/rohanaceres/github/io/mycv/PersonalData.java
@@ -0,0 +1,159 @@
+package rohanaceres.github.io.mycv;
+
+public class PersonalData {
+
+    private static final PersonalData Instance = new PersonalData();
+
+    private String name;
+    private String email;
+    private String district;
+    private String city;
+    private String phoneNumber;
+    private String graduation;
+    private String mba;
+    private String phd;
+    private String companyTime;
+    private String companyName;
+    private String jobTitle;
+    private String courseTitle;
+    private String institution;
+    private String courseTime;
+    private String publicationDate;
+    private String publicationTitle;
+
+    // singleton
+    private PersonalData() {}
+
+    public static PersonalData getInstance() {
+        return Instance;
+    }
+
+    // getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getGraduation() {
+        return graduation;
+    }
+
+    public void setGraduation(String graduation) {
+        this.graduation = graduation;
+    }
+
+    public String getMba() {
+        return mba;
+    }
+
+    public void setMba(String mba) {
+        this.mba = mba;
+    }
+
+    public String getPhd() {
+        return phd;
+    }
+
+    public void setPhd(String phd) {
+        this.phd = phd;
+    }
+
+    public String getCompanyTime() {
+        return companyTime;
+    }
+
+    public void setCompanyTime(String companyTime) {
+        this.companyTime = companyTime;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+
+    public void setCourseTitle(String courseTitle) {
+        this.courseTitle = courseTitle;
+    }
+
+    public String getInstitution() {
+        return institution;
+    }
+
+    public void setInstitution(String institution) {
+        this.institution = institution;
+    }
+
+    public String getCourseTime() {
+        return courseTime;
+    }
+
+    public void setCourseTime(String courseTime) {
+        this.courseTime = courseTime;
+    }
+
+    public String getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(String publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public String getPublicationTitle() {
+        return publicationTitle;
+    }
+
+    public void setPublicationTitle(String publicationTitle) {
+        this.publicationTitle = publicationTitle;
+    }
+}
